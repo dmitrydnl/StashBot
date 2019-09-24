5ce09d27fc867e540d064a61ca039e9dc5209249
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-09-25 01:42:23 +0300

    Read BotToken

---
+++ src/StashBot/app/GetWorkData/BotToken.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace StashBot.GetWorkData
+{
+    internal class BotToken
+    {
+        private const string botTokenFileName = "workData.json";
+        private static string botToken;
+
+        internal BotToken()
+        {
+            if (string.IsNullOrEmpty(botToken))
+            {
+                string text = TextFileIO.Read(botTokenFileName);
+                dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(text);
+                botToken = (string)jsonObject.botToken;
+            }
+        }
+
+        internal string Get()
+        {
+            return botToken;
+        }
+    }
+}
---
+++ src/StashBot/app/GetWorkData/TextFileIO.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace StashBot.GetWorkData
+{
+    internal static class TextFileIO
+    {
+        internal static string Read(string filePath)
+        {
+            string text = File.ReadAllText(filePath);
+            return text;
+        }
+    }
+}
