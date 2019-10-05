23c77cd80a0b981d9f7316ce9246495e0e15c572
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-10-05 22:03:18 +0300

    Create Session and SessionManager

---
+++ src/StashBot/app/ChatSession/Session.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StashBot.ChatSession
+{
+    internal class Session
+    {
+        private readonly long chatId;
+        private DateTime lastUserMessage;
+        private readonly List<int> botMessagesId;
+
+        internal Session(long chatId)
+        {
+            this.chatId = chatId;
+            lastUserMessage = DateTime.UtcNow;
+            botMessagesId = new List<int>();
+        }
+
+        internal void UserMessageReceived()
+        {
+            lastUserMessage = DateTime.UtcNow;
+        }
+
+        internal void BotMessageSent(int messageId)
+        {
+            botMessagesId.Add(messageId);
+        }
+
+        internal long ChatId()
+        {
+            return chatId;
+        }
+
+        internal DateTime LastUserMessage()
+        {
+            return lastUserMessage;
+        }
+
+        internal List<int> BotMessagesId()
+        {
+            return botMessagesId;
+        }
+    }
+}
