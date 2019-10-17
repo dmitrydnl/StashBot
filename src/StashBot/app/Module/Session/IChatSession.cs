ada7699b74f20e197320f85d8e5c33a57d6417e2
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-10-17 16:09:19 +0300

    Update IChatSession.cs

--- src/StashBot/app/Module/Session/IChatSession.cs
+++ src/StashBot/app/Module/Session/IChatSession.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿namespace StashBot.Module.Session
 {
     internal interface IChatSession
     {
         void UserSentMessage(int messageId);
         void BotSentMessage(int messageId);
         void Authorize();
+        void Kill();
+        bool NeedKill();
         long ChatId();
         bool IsAuthorized();
-        DateTime LastUserMessage();
-        List<int> BotMessagesId();
     }
 }
