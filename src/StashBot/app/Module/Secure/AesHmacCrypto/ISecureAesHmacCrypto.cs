509e8cfd33108096f61eead7a6e3072e30cfd01a
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-10-21 12:27:29 +0300

    Create SecureAesHmacCrypto

---
+++ src/StashBot/app/Module/Secure/AesHmacCrypto/ISecureAesHmacCrypto.cs
@@ -0,0 +1,8 @@
+﻿namespace StashBot.Module.Secure.AesHmacCrypto
+{
+    internal interface ISecureAesHmacCrypto
+    {
+        string EncryptWithAesHmac(string secretMessage, string password, byte[] nonSecretPayload = null);
+        string DecryptWithAesHmac(string encryptedMessage, string password, int nonSecretPayloadLength = 0);
+    }
+}
