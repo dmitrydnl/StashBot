fad54a89f90502a696c6ef9d9cc09713c6182e0d
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-11-08 02:12:52 +0300

    Update bot settings

---
+++ src/StashBot/app/BotSettings/DatabaseSettings.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace StashBot.BotSettings
+{
+    internal static class DatabaseSettings
+    {
+        private const string BOT_SETTINGS_FILE_NAME = "BotSettings.json";
+
+        private static bool isSetUp;
+        private static DatabaseType accountDatabaseType;
+        private static DatabaseType stashDatabaseType;
+
+        internal static DatabaseType AccountDatabaseType
+        {
+            get
+            {
+                SetUpSettings();
+                return accountDatabaseType;
+            }
+        }
+
+        internal static DatabaseType StashDatabaseType
+        {
+            get
+            {
+                SetUpSettings();
+                return stashDatabaseType;
+            }
+        }
+
+        private static void SetUpSettings()
+        {
+            if (isSetUp)
+            {
+                return;
+            }
+
+            string text = File.ReadAllText(BOT_SETTINGS_FILE_NAME);
+            dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(text);
+            accountDatabaseType = StringToDatabaseType((string)jsonObject.accountDatabaseType);
+            stashDatabaseType = StringToDatabaseType((string)jsonObject.stashDatabaseType);
+            isSetUp = true;
+        }
+
+        private static DatabaseType StringToDatabaseType(string type)
+        {
+            switch(type)
+            {
+                case "local":
+                    return DatabaseType.Local;
+                case "sqlite":
+                    return DatabaseType.Sqlite;
+                default:
+                    return DatabaseType.Local;
+            }
+        }
+    }
+}
