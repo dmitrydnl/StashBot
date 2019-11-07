ee13034ee71bad5e28c6f62635da253a4c498193
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-11-08 00:11:19 +0300

    Create Sqlite user

---
+++ src/StashBot/app/Module/Database/Account/Sqlite/UserSqlite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using StashBot.Module.Secure;
+
+namespace StashBot.Module.Database.Account.Sqlite
+{
+    public class UserSqlite : IUser
+    {
+        public long ChatId
+        {
+            get;
+            private set;
+        }
+
+        public bool IsAuthorized
+        {
+            get;
+            private set;
+        }
+
+        public string EncryptedPassword
+        {
+            get;
+            private set;
+        }
+
+        private readonly string hashPassword;
+
+        internal UserSqlite(long chatId, string hashPassword)
+        {
+            if (string.IsNullOrEmpty(hashPassword))
+            {
+                throw new ArgumentException("Hash password cannot be null");
+            }
+
+            ChatId = chatId;
+            IsAuthorized = false;
+            this.hashPassword = hashPassword;
+            EncryptedPassword = null;
+        }
+
+        public void Login(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Password cannot be null");
+            }
+
+            ISecureManager secureManager = ModulesManager.GetModulesManager().GetSecureManager();
+
+            EncryptedPassword = secureManager.EncryptWithAes(password);
+            IsAuthorized = true;
+        }
+
+        public void Logout()
+        {
+            EncryptedPassword = null;
+            IsAuthorized = false;
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            ISecureManager secureManager = ModulesManager.GetModulesManager().GetSecureManager();
+
+            return secureManager.CompareWithHash(password, hashPassword);
+        }
+    }
+}
