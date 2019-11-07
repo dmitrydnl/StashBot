1d113a1f3d2079b17bb4c1f3b7fd8dd71ff66f98
Captious99 <48892069+Captious99@users.noreply.github.com>
2019-11-08 01:42:51 +0300

    Create users database

---
+++ src/StashBot/Migrations/20191107214740_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StashBot.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserModelId = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChatId = table.Column<long>(nullable: false),
+                    HashPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserModelId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ src/StashBot/app/Module/Database/Account/Sqlite/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StashBot.Module.Database.Account.Sqlite
+{
+    public class UserModel
+    {
+        public long UserModelId { get; set; }
+        public long ChatId { get; set; }
+        public string HashPassword { get; set; }
+    }
+}
