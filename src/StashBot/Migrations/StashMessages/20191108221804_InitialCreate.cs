c39a57e64d3e16a3c5bb4cc3b4a722bd24277136
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-11-09 01:23:00 +0300

    Create StashMessages database

---
+++ src/StashBot/Migrations/StashMessages/20191108221804_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StashBot.Migrations.StashMessages
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StashMessages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChatId = table.Column<long>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StashMessages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StashMessages");
+        }
+    }
+}
