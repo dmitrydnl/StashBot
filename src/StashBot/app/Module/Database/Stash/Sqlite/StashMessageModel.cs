a40155a804a2b2b44d6d366bf7406a1cfae220a1
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-11-09 00:43:03 +0300

    Create StashMessageModel.cs

---
+++ src/StashBot/app/Module/Database/Stash/Sqlite/StashMessageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StashBot.Module.Database.Stash.Sqlite
+{
+    public class StashMessageModel
+    {
+        public long StashMessageModelId { get; set; }
+        public long ChatId { get; set; }
+        public StashMessageType Type { get; set; }
+        public string Content { get; set; }
+    }
+}
