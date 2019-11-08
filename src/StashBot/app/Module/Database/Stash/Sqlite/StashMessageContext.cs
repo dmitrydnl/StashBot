cd1095239f2ea09f16b51bb56568dfeb210f6a51
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-11-09 00:43:33 +0300

    Create StashMessageContext.cs

---
+++ src/StashBot/app/Module/Database/Stash/Sqlite/StashMessageContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StashBot.Module.Database.Stash.Sqlite
+{
+    public class StashMessageContext : DbContext
+    {
+        public DbSet<StashMessageModel> Stashes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=stashes.db");
+        }
+    }
+}
