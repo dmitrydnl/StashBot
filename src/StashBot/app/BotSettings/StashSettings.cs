e4cc6f5dbc25caa63e981be4110966c19c948152
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-11-19 23:13:53 +0300

    Add stash settings

---
+++ src/StashBot/app/BotSettings/StashSettings.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace StashBot.BotSettings
+{
+    internal static class StashSettings
+    {
+        private const string BOT_SETTINGS_FILE_NAME = "BotSettings.json";
+
+        private static bool isSetUp;
+        private static int stashMessageLimit;
+
+        internal static int StashMessageLimit
+        {
+            get
+            {
+                SetUpSettings();
+                return stashMessageLimit;
+            }
+        }
+
+        private static void SetUpSettings()
+        {
+            if (isSetUp)
+            {
+                return;
+            }
+
+            string text = File.ReadAllText(BOT_SETTINGS_FILE_NAME);
+            dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(text);
+            stashMessageLimit = (int)jsonObject.stashMessageLimit;
+            isSetUp = true;
+        }
+    }
+}
