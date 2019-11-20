daf936a20d091be5cccfc0afd3adb0f7f5d3d291
dmitrydnl <48892069+Captious99@users.noreply.github.com>
2019-11-21 00:07:15 +0300

    command name

--- src/StashBot/app/Module/Message/Handler/ChatStateHandler/AuthorizedStateHandler.cs
+++ src/StashBot/app/Module/Message/Handler/ChatStateHandler/AuthorizedStateHandler.cs
@@ -20,7 +20,7 @@ namespace StashBot.Module.Message.Handler.ChatStateHandler
 
         private void InitializeCommands()
         {
-            chatCommands.Add("/stash", GetStash);
+            chatCommands.Add("/Stash", GetStash);
             chatCommands.Add("/logout", Logout);
             chatCommands.Add("/exit", Exit);
         }
